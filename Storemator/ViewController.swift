f10597ff335cc775d03fcf2f514798f1c43609a3
Hamletdev <hamletdev001@gmail.com>
2020-12-24 09:29:00 +0530

    Initial Commit

---
+++ Storemator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storemator
+//
+//  Created by Amit Chaudhary on 12/24/20.
+//  Copyright © 2020 Amit Chaudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
